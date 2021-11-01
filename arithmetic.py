8bc9fd56a23bbb509afd2a9a3d0c32f1821332e8
konradkam85 <konradkamieniecki@gmail.com>
2021-11-01 21:28:03 +0100

    Added main WHILE loop

--- arithmetic.py
+++ arithmetic.py
@@ -1,1 +1,32 @@
-print('starting a new program...')
+import random
+
+def addition(x,y):
+    print(f'{x} + {y} = ')
+    return x + y
+
+c = None    # chart in while loop condition
+
+# Loop untill pressing 'q'
+while c != 'q':
+    x = random.randrange(0,20,1)
+    y = random.randrange(0,10,1)
+
+    result = addition(x,y)
+    answer = input()
+
+    ## Test if the input is an integer
+    try:
+        answer=int(answer)
+    except ValueError:
+        try:
+            answer = float(answer)
+            print('This is a float. Should be an integer.') 
+        except ValueError:
+            print(f'{answer} is not a number.')
+    
+    if result == answer:
+        print('Success!')
+    else:
+        print('Try again...')
+
+    c = input('Press ENTER for next or q to quit: ')
